8b6ba327ef68dbfd10d0710762bc2b80bfbf77a4
NasirZpc <1041870188@qq.com>
2018-12-13 10:24:52 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        link-GY-app
+      </h1>
+      <h2 class="subtitle">
+        My astounding Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
