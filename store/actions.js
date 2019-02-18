faa315a85374d980201942f1057eb1e0be3f5648
NasirZpc <1041870188@qq.com>
2019-02-18 11:36:32 +0800

    seo优化

--- store/actions.js
+++ store/actions.js
@@ -1,38 +1,41 @@
 import qs from 'qs'
 export default{
     async nuxtServerInit ({ commit }, { req, app }) {
-        if(req.headers.cookie){
-            var cookieArr = req.headers.cookie.split("; ")
-            var obj = {},
-                active = {},
-                house = {};
-            for(var i=0;i<cookieArr.length;i++){
-                if(cookieArr[i].split('=')[0] == 'linkId'){//获取账户id
-                    obj[cookieArr[i].split('=')[0]] = cookieArr[i].split('linkId=')[1]
+        try{
+            if(req.headers.cookie){
+                var cookieArr = req.headers.cookie.split("; ")
+                var obj = {},
+                    active = {},
+                    house = {};
+                for(var i=0;i<cookieArr.length;i++){
+                    if(cookieArr[i].split('=')[0] == 'linkId'){//获取账户id
+                        obj[cookieArr[i].split('=')[0]] = cookieArr[i].split('linkId=')[1]
+                    }
+                    if(cookieArr[i].split('=')[0] == 'activeIndex'){
+                        active[cookieArr[i].split('=')[0]] = cookieArr[i].split('activeIndex=')[1]
+                    }
+                    if(cookieArr[i].split('=')[0] == 'houseActive'){
+                        house[cookieArr[i].split('=')[0]] = cookieArr[i].split('houseActive=')[1]
+                    }
                 }
-                if(cookieArr[i].split('=')[0] == 'activeIndex'){
-                    active[cookieArr[i].split('=')[0]] = cookieArr[i].split('activeIndex=')[1]
+                if (obj && obj.linkId) {
+                    const linkId =obj.linkId;
+                    // await app.$axios.post('/Member/QueryInfo',{//获取个人信息
+                    //     AccountId:obj.linkId
+                    // }).then(res=>{
+                    //     commit('SET_USERINFO',res.data.Status===200?qs.parse(res.data.Data):'')
+                    // })
+                }else{
+                    commit('SET_USERINFO','')
                 }
-                if(cookieArr[i].split('=')[0] == 'houseActive'){
-                    house[cookieArr[i].split('=')[0]] = cookieArr[i].split('houseActive=')[1]
-                }
-            }
-            if (obj && obj.linkId) {
-                const linkId =obj.linkId;
-                // await app.$axios.post('/Member/QueryInfo',{//获取个人信息
-                //     AccountId:obj.linkId
-                // }).then(res=>{
-                //     commit('SET_USERINFO',res.data.Status===200?qs.parse(res.data.Data):'')
-                // })
+                commit('SET_ACTIVEINDEX',active.activeIndex)
+                commit('SET_HOUSEACTIVE',house.houseActive)
             }else{
                 commit('SET_USERINFO','')
             }
-            commit('SET_ACTIVEINDEX',active.activeIndex)
-            commit('SET_HOUSEACTIVE',house.houseActive)
-        }else{
-            commit('SET_USERINFO','')
+        }catch(e){
+            console.log(e)
         }
-
     },
     SET_ACTIVEINDEX({commit},activeIndex){
         commit('SET_ACTIVEINDEX',activeIndex)
